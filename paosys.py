5b955dfaee8a88a5419fe3630d78eea4e710461e
guam68 <guam_68@hotmail.com>
2019-05-15 16:24:17 -0700

    working flags for mode

--- paosys.py
+++ paosys.py
@@ -18,7 +18,9 @@ def encode(excel, num, mode):
     num = list(num)
     encoded = ''
     sheet = excel['Sheet1']
-    size = 4 if mode else 6
+    size = 4 if mode != 'pao' else 6
+    col1 = 'B' if mode[0] == 'p' else 'C'
+    col2 = 'C' if mode[1] == 'a' else 'D'
 
     while len(num) > 0:
         chunks.append(num[:size])
@@ -26,25 +28,22 @@ def encode(excel, num, mode):
     
     for chunk in chunks:
         person = str(int(''.join(chunk[:2]))+1) if len(chunk[:2]) == 2 else str(int(chunk[0])+1)
-        encoded += sheet['E' + person].value if len(chunk[:2]) == 1 else sheet['B'+ person].value
+        encoded += sheet['E' + person].value + ' ' if len(chunk[:2]) == 1 else sheet[col1 + person].value + ' '
 
         if len(chunk) > 2:
             action = str(int(''.join(chunk[2:4]))+1) if len(chunk[2:4]) == 2 else str(int(chunk[2])+1)
-            encoded += sheet['E' + action].value if len(chunk[2:4]) == 1 else sheet['C'+ action].value
-            if mode: encoded += '\t'
+            encoded += sheet['E' + action].value + ' ' if len(chunk[2:4]) == 1 else sheet[col2 + action].value + ' '
+            if mode != 'pao': encoded += '\n'
         if len(chunk) > 4:
             obj = str(int(''.join(chunk[4:6]))+1) if len(chunk[4:6]) == 2 else str(int(chunk[4])+1)
-            encoded += sheet['E' + obj].value if len(chunk[4:6]) == 1 else sheet['D'+ obj].value + '\t'
-
-
+            encoded += sheet['E' + obj].value + ' ' if len(chunk[4:6]) == 1 else sheet['D'+ obj].value + '\n'
 
     print(encoded)
 
 
-
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-m', dest='mode')
+parser.add_argument('-m', dest='mode', default='pao')
 parser.add_argument('-n', dest='num')
 parser.add_argument('-f', dest='file')
 parser.add_argument('-r', nargs='*', dest='replace')
